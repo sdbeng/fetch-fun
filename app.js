e519a55080c23530c54549b16da777be0dc36d68
Sergio D. Barrera <sdbeng@gmail.com>
2022-01-30 08:03:06 -0800

    fix import rqmt and update package json

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+import fetch from 'node-fetch'
+// const fetch = require("node-fetch")
 console.log(`It works!`);
+
+const fetcher = async () => {
+    const response = await fetch('https://api.github.com/users/github')
+    const data = await response.json()
+    
+    console.log(data);
+
+}
+
+fetcher()
